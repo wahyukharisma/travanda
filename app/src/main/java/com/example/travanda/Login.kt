ffd9614f79c8b6d8162e027f5be7c4d5751b558a
nandiarani <nandia_rani@outlook.com>
2019-05-27 15:44:30 +0700

    Merge login & login UI

--- app/src/main/java/com/example/travanda/Login.kt
+++ app/src/main/java/com/example/travanda/Login.kt
@@ -15,9 +15,9 @@ class Login : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
-        button_login.setOnClickListener {
-            val email= input_email.text.toString()
-            val password= input_password.text.toString()
+        btn_login.setOnClickListener {
+            val email= et_email.text.toString()
+            val password= et_password.text.toString()
             if(email.isEmpty()||password.isEmpty()){
                 Toast.makeText(this,"Please insert email and password",Toast.LENGTH_SHORT).show()
                 return@setOnClickListener
@@ -27,8 +27,6 @@ class Login : AppCompatActivity() {
                 .addOnCompleteListener {
                     if(!it.isSuccessful){
                         return@addOnCompleteListener
-                        val intent= Intent(this, MainActivity::class.java)
-                        startActivity(intent)
                     }
                     else
                         Toast.makeText(this,"Successfully login!",Toast.LENGTH_SHORT).show()
