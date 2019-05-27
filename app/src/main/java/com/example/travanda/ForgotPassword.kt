5820ca6319ecceaaedf9ab357f81cab358209766
nandiarani <nandia_rani@outlook.com>
2019-05-27 17:07:11 +0700

    Init ForgotPassword

---
+++ app/src/main/java/com/example/travanda/ForgotPassword.kt
@@ -0,0 +1,11 @@
+package com.example.travanda
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+
+class ForgotPassword : AppCompatActivity(){
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.forgot_password_landing)
+    }
+}
