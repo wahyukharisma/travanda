5355c4f146259476feb07ca339fb2054ab6b5a25
nandiarani <nandia_rani@outlook.com>
2019-06-09 17:07:04 +0700

    landing page backend done

---
+++ app/src/main/java/com/example/travanda/IntroViewPagerAdapter.kt
@@ -0,0 +1,43 @@
+package com.example.travanda
+
+import android.content.Context
+import android.support.annotation.NonNull
+import android.support.v4.view.PagerAdapter
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+
+
+class IntroViewPagerAdapter(val mListScreen: List<ScreenItem>):PagerAdapter(){
+
+    override fun isViewFromObject(v: View, `object`: Any): Boolean {
+        return v===`object` as View
+    }
+
+    override fun getCount(): Int {
+        return mListScreen.size
+    }
+
+    override fun instantiateItem(parent: ViewGroup, position: Int): Any {
+        val view=LayoutInflater.from(parent?.context)
+            .inflate(R.layout.item_landing_scene,parent,false)
+
+        val intro_title:TextView=view.findViewById(R.id.intro_title)
+        val intro_desc:TextView=view.findViewById(R.id.intro_description)
+        val intro_img:ImageView=view.findViewById(R.id.intro_img)
+
+        intro_title.text=mListScreen.get(position).title
+        intro_desc.text=mListScreen.get(position).description
+        intro_img.setImageResource(R.drawable.ic_email_blue_24dp)
+
+        parent?.addView(view)
+
+        return view
+    }
+
+    override fun destroyItem(parent: ViewGroup, position: Int, `object`: Any) {
+        parent.removeView(`object` as View)
+    }
+}
--- app/src/main/java/com/example/travanda/OnLanding.kt
+++ app/src/main/java/com/example/travanda/OnLanding.kt
@@ -2,12 +2,21 @@ package com.example.travanda
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.support.v4.view.PagerAdapter
+import kotlinx.android.synthetic.main.landing_scene.*
 
 class OnLanding : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.landing_scene)
+        val item1:ScreenItem= ScreenItem("text1","test2",R.drawable.ic_email_blue_24dp)
+        val item2:ScreenItem=ScreenItem("text2","test2",R.drawable.ic_arrow_back_black_24dp)
+        val item3:ScreenItem=ScreenItem("text3","test2",R.drawable.ic_mtrl_chip_checked_black)
+        val items= listOf<ScreenItem>(item1,item2,item3)
+        val adapter=IntroViewPagerAdapter(items)
+        viewPager.adapter=adapter
+
     }
 
 
