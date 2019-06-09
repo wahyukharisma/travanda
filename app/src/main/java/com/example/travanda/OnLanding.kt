285f3f099790b2e0a60e0f27306bf57e3775f1a0
Wahyu Kharisma <madewahyu39G@gmail.com>
2019-06-09 13:56:20 +0700

    Adding new Landing Page UI
    + adding new layout
    + adding new kotlin class

---
+++ app/src/main/java/com/example/travanda/OnLanding.kt
@@ -0,0 +1,14 @@
+package com.example.travanda
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+
+class OnLanding : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.landing_scene)
+    }
+
+    
+}
